cdc3a68efde308214046fe2654d547d619e1ac62
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-13 21:51:23 -0300

    chore: change tailwind theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss';
+const {
+    tailwindColorsConfig: colors,
+    tailwindScreensConfig: screens,
+    tailwindShadowsConfig: shadows,
+    tailwindSpacingConfig: spacing,
+    tailwindFontFamilyConfig: fontFamily,
+    tailwindFontSizeConfig: fontSize,
+    tailwindFontWeightConfig: fontWeight,
+} = require('./src/styles/theme');
 
-const config: Config = {
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
-        extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic':
-                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-            },
-        },
+        extend: {},
+        colors,
+        screens,
+        boxShadow: shadows,
+        spacing,
+        fontFamily,
+        fontSize,
+        fontWeight,
     },
     plugins: [],
+    darkMode: 'class',
 };
-export default config;
