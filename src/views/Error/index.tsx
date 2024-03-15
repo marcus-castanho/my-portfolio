44ea04cdf408a9c3dc36b2152d1ab8d1328d34c8
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-15 14:08:33 -0300

    feat: use PageContainer and Main components in Error page

--- src/views/Error/index.tsx
+++ src/views/Error/index.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 import { ClientWrapper } from './components/ClientWrapper';
 import { ResetButton } from './components/ResetButton';
+import { PageContainer } from '@/components/PageContainer';
+import { Main } from '@/components/Main';
 
 export type ErrorProps = {
     error: Error;
@@ -11,21 +13,27 @@ export type ErrorProps = {
 export const Error: FC<ErrorProps> = ({ error, reset }) => {
     return (
         <ClientWrapper error={error}>
-            <div className="h-screen">
-                <div className="flex h-full w-full items-center justify-center">
-                    <div className="flex flex-col items-center">
-                        <h1 className="flex justify-center px-6 py-10 text-center text-3xl font-bold sm:text-left">
-                            Sorry, something went wrong
-                        </h1>
-                        <div className="pb-10">
-                            <ResetButton reset={reset} />
+            <PageContainer>
+                <Main>
+                    <div className="flex w-full items-center justify-center">
+                        <div className="flex flex-col items-center">
+                            <h1 className="flex justify-center px-6 py-10 text-center text-3xl font-bold sm:text-left">
+                                Sorry, something went wrong
+                            </h1>
+                            <div className="pb-10">
+                                <ResetButton reset={reset} />
+                            </div>
+                            <Link
+                                href="/"
+                                target="_self"
+                                aria-label="home-link"
+                            >
+                                Go Home
+                            </Link>
                         </div>
-                        <Link href="/" target="_self" aria-label="home-link">
-                            Go Home
-                        </Link>
                     </div>
-                </div>
-            </div>
+                </Main>
+            </PageContainer>
         </ClientWrapper>
     );
 };
