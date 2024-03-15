f006975ed37b7b29691f763338259afe1f017ebb
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-15 14:02:21 -0300

    feat: use views/Error in GlobalError wrapper

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,7 +1,19 @@
 'use client';
 
 import React from 'react';
+import { Error } from '@/views/Error';
 
-export default function GlobalError() {
-    return <></>;
+export type GlobalErrorProps = {
+    error: Error;
+    reset: () => void;
+};
+
+export default function GlobalError({ error, reset }: GlobalErrorProps) {
+    return (
+        <html>
+            <body>
+                <Error error={error} reset={reset} />
+            </body>
+        </html>
+    );
 }
