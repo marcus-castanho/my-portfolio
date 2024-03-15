706382610065e35085c59125039a37ad14203d0d
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-15 14:01:10 -0300

    feat: use views/Error in error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,19 @@
 'use client';
 
 import React from 'react';
+import { Error as ErrorPage } from '@/views/Error';
 
-export default function Error() {
-    return <></>;
+export type ErrorProps = {
+    error: Error;
+    reset: () => void;
+};
+
+export default function Error({ error, reset }: ErrorProps) {
+    return (
+        <html>
+            <body>
+                <ErrorPage error={error} reset={reset} />
+            </body>
+        </html>
+    );
 }
