f5c5c6698af35f878e73f4cffa45642e8a59dc83
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-15 14:13:19 -0300

    feat: create InternalServerError page

--- src/views/InternalServerError.tsx
+++ src/views/InternalServerError.tsx
@@ -1,5 +1,31 @@
-import React, { FC } from 'react';
+import React from 'react';
+import { PageContainer } from '@/components/PageContainer';
+import { Main } from '@/components/Main';
+import Link from 'next/link';
 
 export const InternalServerError = () => {
-    return <></>;
+    return (
+        <PageContainer>
+            <Main>
+                <div className="flex w-full items-center justify-center">
+                    <div className="flex flex-col items-center">
+                        <h1 className="flex justify-center px-6 pb-4 pt-10 text-center text-5xl font-bold sm:text-left">
+                            500 - Internal server error
+                        </h1>
+                        <p className="px-6 pb-10">
+                            Sorry, something went wrong
+                        </p>
+                        <Link
+                            href="/"
+                            target="_self"
+                            aria-label="home-link"
+                            className="hover:underline"
+                        >
+                            Go Home
+                        </Link>
+                    </div>
+                </div>
+            </Main>
+        </PageContainer>
+    );
 };
