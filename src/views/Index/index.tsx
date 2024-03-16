ca88839ce080a533c75bdd8b2b5790be434d5205
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-16 17:10:12 -0300

    chore: change presentation text

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -24,9 +24,9 @@ const Presentation = () => {
     return (
         <div className="flex flex-col gap-6 p-12">
             <p className="text-lg">Hi!🖐</p>
-            <h1 className="text-5xl">Marcus</h1>
+            <h1 className="text-5xl">{`I'm Marcus`}</h1>
             <p className="text-lg">
-                {`I'm a software developer from Brazil who likes to build
+                {`I am a software developer from Brazil who likes to build
         scalable back end services and user friendly front
         end applications.`}
             </p>
