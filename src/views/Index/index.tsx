fcf95d03b425506fd69cd70c41d548dfa47e9ae3
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-18 11:47:12 -0300

    feat: style mobile layout for CardsGrid

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -22,20 +22,20 @@ const ProfileHero = () => {
 
 const CardsGrid = () => {
     return (
-        <div className="grid h-full grid-cols-[20%_20%_30%_30%] grid-rows-2">
-            <div className="col-span-1 row-span-2">
+        <div className="grid grid-cols-2 grid-rows-4 sm:h-full sm:grid-cols-[20%_20%_30%_30%] sm:grid-rows-2">
+            <div className="col-span-2 row-span-1 sm:col-span-1 sm:row-span-2">
                 <GreetingsCard />
             </div>
-            <div className="col-span-1 row-span-2">
+            <div className="col-span-1 row-span-2 sm:col-span-1 sm:row-span-2">
                 <AboutCard />
             </div>
-            <div className="col-span-1 row-span-1">
+            <div className="col-span-1 row-span-1 sm:col-span-1 sm:row-span-1">
                 <WorksCard />
             </div>
-            <div className="col-span-1 row-span-1">
+            <div className="col-span-1 row-span-1 sm:col-span-1 sm:row-span-1">
                 <BlogCard />
             </div>
-            <div className="col-span-2 row-span-1">
+            <div className="col-span-2 row-span-1 sm:col-span-2 sm:row-span-1">
                 <ContactCard />
             </div>
         </div>
