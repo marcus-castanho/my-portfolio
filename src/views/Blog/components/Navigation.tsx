349583fc1a42b696b35852b2768faa7d020aab5a
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-19 02:12:40 -0300

    feat: adapt Navigation component for SSR navigation

--- src/views/Blog/components/Navigation.tsx
+++ src/views/Blog/components/Navigation.tsx
@@ -3,18 +3,40 @@
 import React, { FC } from 'react';
 import { usePagination } from '@/hooks/usePagination';
 import { PaginationNav } from '@/components/PaginationNav';
+import { usePathname, useRouter } from 'next/navigation';
+import { match } from 'ts-pattern';
 
 type NavigationProps = {
+    initialPage: number;
     totalPages: number;
 };
 
-export const Navigation: FC<NavigationProps> = ({ totalPages }) => {
-    const { page, changePage, getPagesIndexes } = usePagination();
+export const Navigation: FC<NavigationProps> = ({
+    initialPage,
+    totalPages,
+}) => {
+    const pathname = usePathname();
+    const router = useRouter();
+    const { page, getPagesIndexes } = usePagination(initialPage);
 
     return (
         <PaginationNav
             page={page}
-            changePage={changePage}
+            changePage={(selectedPage) => {
+                const pageToGo = match(selectedPage)
+                    .with('previous', () => {
+                        const previousPage = page - 1;
+                        return previousPage <= 0 ? 1 : previousPage;
+                    })
+                    .with('next', () => {
+                        const previousPage = page + 1;
+                        return previousPage >= totalPages
+                            ? totalPages
+                            : previousPage;
+                    })
+                    .otherwise(() => selectedPage);
+                router.push(`${pathname}?page=${pageToGo}`);
+            }}
             pagesIndexes={getPagesIndexes(totalPages, 5).indexesArr}
         />
     );
