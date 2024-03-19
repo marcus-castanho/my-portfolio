c0c28fb81ac1037f163f65bc1b47fcc717e6e3e1
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-18 23:35:57 -0300

    feat: create types for devToApi service

---
+++ src/services/devToApi/index.ts
@@ -0,0 +1,25 @@
+export type ReturnValue<T = never> =
+    | {
+          success: true;
+          status: number;
+          data: [T] extends [never] ? null : T;
+      }
+    | {
+          success: false;
+          status: number;
+          data: null;
+      };
+
+export type FetchType =
+    | {
+          type: 'SSR';
+          options: { cache: NonNullable<RequestCache> };
+      }
+    | {
+          type: 'SSG';
+          options: { next: NextFetchRequestConfig };
+      };
+
+export type Fetch<R, T = never> = [T] extends [never]
+    ? (fetchType?: FetchType) => Promise<ReturnValue<R>>
+    : (payload: T, fetchType?: FetchType) => Promise<ReturnValue<R>>;
