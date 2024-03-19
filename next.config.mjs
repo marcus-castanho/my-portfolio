2eef20ecaa1915a898593116a237a992ce7b1534
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-18 21:56:46 -0300

    feat: add next config to allow download images from DEV.to CDN host

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            /**DEV.to CDN host */
+            {
+                protocol: 'https',
+                hostname: 'media.dev.to',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
