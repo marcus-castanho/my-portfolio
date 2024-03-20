8d2ac9b9f94d7314f54f9d2b3048cde99244273f
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-19 23:02:19 -0300

    feat: use img in DEVtoLogo

--- src/components/DEVtoLogo.tsx
+++ src/components/DEVtoLogo.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { DEVtoIcon } from './icons/DEVtoIcon';
+import React, { FC } from 'react';
+import Image from 'next/image';
+import { match } from 'ts-pattern';
 
-export const DEVtoLogo = () => {
+type DEVtoLogoProps = {
+    size: 8 | 16 | 24 | 32 | 36 | 40 | 48;
+};
+
+export const DEVtoLogo: FC<DEVtoLogoProps> = ({ size }) => {
     return (
-        <div className="relative">
-            <div className="absolute bottom-0 right-full z-10 h-6 w-6 translate-x-[-0.25rem] translate-y-[-0.25rem] bg-white" />
-            <div className="absolute bottom-0 right-full z-20">
-                <DEVtoIcon fillColor="black" size={32} />
-            </div>
+        <div
+            className={match(size)
+                .with(8, () => 'min-w- relative min-h-1 min-w-1')
+                .with(16, () => 'min-w- relative min-h-4 min-w-4')
+                .with(24, () => 'min-w- relative min-h-6 min-w-6')
+                .with(32, () => 'min-w- relative min-h-8 min-w-8')
+                .with(36, () => 'min-w- relative min-h-9 min-w-9')
+                .with(40, () => 'min-w- relative min-h-10 min-w-10')
+                .with(48, () => 'min-w- relative min-h-12 min-w-12')
+                .otherwise(() => 'min-w- relative min-h-9 min-w-9')}
+        >
+            <Image
+                src="/assets/dev-black.png"
+                alt="DEV-to-logo"
+                fill
+                className="object-contain"
+            />
         </div>
     );
 };
