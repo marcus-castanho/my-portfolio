a1f29b0f51d7e22b1826891dbff54c494f0e24d8
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-20 01:20:50 -0300

    feat: use NoContentMessage in works page

--- src/views/Works.tsx
+++ src/views/Works.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Main } from '@/components/Main';
+import { Header } from '@/components/Header';
+import { PageTitle } from '@/components/PageTitle';
+import { NoContentMessage } from '@/components/NoContentMessage';
 
 export const Works = () => {
-    return <></>;
+    return (
+        <Main>
+            <div className="flex h-full flex-col">
+                <Header />
+                <PageTitle title="works" />
+                <div className="flex flex-1 items-center justify-center">
+                    <NoContentMessage />
+                </div>
+            </div>
+        </Main>
+    );
 };
