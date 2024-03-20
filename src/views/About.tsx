6df9c595fb39f6c68b0c2db27ed380dae969a4dc
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-20 01:15:06 -0300

    feat: use NoContentMessage in about page

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Header } from '@/components/Header';
+import { Main } from '@/components/Main';
+import { PageTitle } from '@/components/PageTitle';
+import { NoContentMessage } from '@/components/NoContentMessage';
 
 export const About = () => {
-    return <></>;
+    return (
+        <Main>
+            <div className="flex h-full flex-col">
+                <Header />
+                <PageTitle title="about" />
+                <div className="flex flex-1 items-center justify-center">
+                    <NoContentMessage />
+                </div>
+            </div>
+        </Main>
+    );
 };
