e64786734d2372c645baae671ef7c5d6d5a5c3fb
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-20 23:48:12 -0300

    fix: remove unused authToken from devToApi httpClient

--- src/services/devToApi/httpClient.ts
+++ src/services/devToApi/httpClient.ts
@@ -3,7 +3,6 @@ import { PROXY_URL } from './proxy';
 
 export function request({
     path,
-    authToken,
     options,
     fetchType = {
         type: 'SSG',
@@ -13,26 +12,13 @@ export function request({
     },
 }: {
     path: string;
-    authToken?: string;
     options?: RequestInit;
     fetchType?: FetchType;
 }) {
     const apiUrl = PROXY_URL;
-    const headers = options?.headers;
-
-    if (!authToken) {
-        return fetch(`${apiUrl}${path}`, {
-            ...options,
-            ...fetchType.options,
-        });
-    }
 
     return fetch(`${apiUrl}${path}`, {
         ...options,
-        headers: {
-            Authorization: `Bearer ${authToken}`,
-            ...headers,
-        },
         ...fetchType.options,
     });
 }
