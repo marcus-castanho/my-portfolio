2143e0bff4dc07508b5710aea733685101e8e2b4
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-03-27 22:04:46 -0300

    feat: move workts page to /Work and use ProjectsList

--- src/views/Works/index.tsx
+++ src/views/Works/index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Main } from '@/components/Main';
 import { Header } from '@/components/Header';
 import { PageTitle } from '@/components/PageTitle';
-import { NoContentMessage } from '@/components/NoContentMessage';
+import { ProjectsList } from './components/ProjectsList';
 
 export const Works = () => {
     return (
         <Main>
-            <div className="flex h-full flex-col">
+            <div className="flex h-full flex-col items-center">
                 <Header />
                 <PageTitle title="works" />
-                <div className="flex flex-1 items-center justify-center">
-                    <NoContentMessage />
+                <div className="flex flex-1 flex-col sm:max-w-[90%]">
+                    <div className="flex w-full flex-1 flex-col items-center p-8">
+                        <ProjectsList />
+                    </div>
                 </div>
             </div>
         </Main>
