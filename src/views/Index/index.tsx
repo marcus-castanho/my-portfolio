a8dac9f8ab4845a0bd22ba594a4ab9cbd88a5931
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-11-10 01:41:30 -0300

    feat: improve mobile responsiviness for tablet screens

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -10,11 +10,11 @@ import { Main } from '@/components/Main';
 
 const ProfileHero = () => {
     return (
-        <div className="flex h-full max-sm:flex-col-reverse max-sm:items-center max-sm:justify-center max-sm:pb-8">
+        <div className="flex h-full p-1 max-sm:flex-col-reverse max-sm:items-center max-sm:justify-center max-sm:pb-8">
             <div className="flex w-[40%] items-center justify-center">
                 <ProfilePicture />
             </div>
-            <div className="flex w-[90%] items-center sm:w-[60%]">
+            <div className="flex w-[90%] items-center lg:landscape:w-[60%]">
                 <Presentation />
             </div>
         </div>
@@ -23,20 +23,20 @@ const ProfileHero = () => {
 
 const CardsGrid = () => {
     return (
-        <div className="grid grid-cols-2 grid-rows-4 sm:h-full sm:grid-cols-[20%_20%_30%_30%] sm:grid-rows-2">
-            <div className="col-span-2 row-span-1 sm:col-span-1 sm:row-span-2">
+        <div className="grid grid-cols-2 grid-rows-4 lg:landscape:h-full lg:landscape:grid-cols-[20%_20%_30%_30%] lg:landscape:grid-rows-2">
+            <div className="col-span-2 row-span-1 lg:landscape:col-span-1 lg:landscape:row-span-2">
                 <GreetingsCard />
             </div>
-            <div className="col-span-1 row-span-2 sm:col-span-1 sm:row-span-2">
+            <div className="col-span-1 row-span-2 lg:landscape:col-span-1 lg:landscape:row-span-2">
                 <AboutCard />
             </div>
-            <div className="col-span-1 row-span-1 sm:col-span-1 sm:row-span-1">
+            <div className="col-span-1 row-span-1 lg:landscape:col-span-1 lg:landscape:row-span-1">
                 <WorksCard />
             </div>
-            <div className="col-span-1 row-span-1 sm:col-span-1 sm:row-span-1">
+            <div className="col-span-1 row-span-1 lg:landscape:col-span-1 lg:landscape:row-span-1">
                 <BlogCard />
             </div>
-            <div className="col-span-2 row-span-1 sm:col-span-2 sm:row-span-1">
+            <div className="col-span-2 row-span-1 lg:landscape:col-span-2 lg:landscape:row-span-1">
                 <ContactCard />
             </div>
         </div>
@@ -47,10 +47,10 @@ export const Index = () => {
     return (
         <Main>
             <div className="flex h-full flex-col">
-                <div className="sm:h-[60%]">
+                <div className="lg:landscape:h-[60%]">
                     <ProfileHero />
                 </div>
-                <div className="sm:h-[40%]">
+                <div className="lg:landscape:h-[40%]">
                     <CardsGrid />
                 </div>
             </div>
