98e3227f7d62e95f9f090e391c9430cd27ece612
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-11-10 02:10:11 -0300

    feat: improve mobile portrait responsiviness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ type MainProps = {
 };
 export const Main: FC<MainProps> = ({ children }) => {
     return (
-        <main className="landscape:h-[200dvh] md:landscape:h-[100dvh]">
+        <main className="portrait:h-[100dvh] landscape:h-[200dvh] md:landscape:h-[100dvh]">
             {children}
         </main>
     );
