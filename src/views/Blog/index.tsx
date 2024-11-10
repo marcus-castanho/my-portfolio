e84dad6a704ec37b55331fe9bcf4e6b1f6e9c9ef
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-11-10 02:00:55 -0300

    feat: use LoadingBlogPosts page and Suspense API in Blog page

--- src/views/Blog/index.tsx
+++ src/views/Blog/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, Suspense } from 'react';
 import { Main } from '@/components/Main';
 import { Header } from '@/components/Header';
 import { Article, getArticles } from '@/services/devToApi/routes/getArticles';
@@ -7,11 +7,9 @@ import { Navigation } from './components/Navigation';
 import { ArticlesList } from './components/ArticlesList';
 import { PageTitle } from '@/components/PageTitle';
 import { NoContentMessage } from '@/components/NoContentMessage';
+import { LoadingBlogPosts } from './LoadingBlogPosts';
 
-type BlogProps = {
-    page: number;
-};
-export const Blog: FC<BlogProps> = async ({ page }) => {
+const SuspenseBlogPosts: FC<BlogProps> = async ({ page }) => {
     const DEV_TO_PROFILE_USERNAME = 'marcus_castanho';
     const ITEMS_PER_PAGE = 12;
 
@@ -58,26 +56,35 @@ export const Blog: FC<BlogProps> = async ({ page }) => {
     const dinamicTotalPages = isLastPage ? page : page + 1;
     const pageIsEmpty = articles.items.length === 0;
 
+    if (pageIsEmpty)
+        return (
+            <div className="flex flex-1 justify-center">
+                <NoContentMessage />
+            </div>
+        );
+
+    return (
+        <div className="flex flex-1 flex-col sm:max-w-[90%]">
+            <div className="flex flex-1 flex-col p-8">
+                <ArticlesList articles={articles.items} />
+                <Navigation initialPage={page} totalPages={dinamicTotalPages} />
+            </div>
+        </div>
+    );
+};
+
+type BlogProps = {
+    page: number;
+};
+export const Blog: FC<BlogProps> = async (props) => {
     return (
         <Main>
             <div className="flex h-full flex-col items-center">
                 <Header />
                 <PageTitle title="blog" />
-                {pageIsEmpty ? (
-                    <div className="flex flex-1 justify-center">
-                        <NoContentMessage />
-                    </div>
-                ) : (
-                    <div className="flex flex-1 flex-col sm:max-w-[90%]">
-                        <div className="flex flex-1 flex-col p-8">
-                            <ArticlesList articles={articles.items} />
-                            <Navigation
-                                initialPage={page}
-                                totalPages={dinamicTotalPages}
-                            />
-                        </div>
-                    </div>
-                )}
+                <Suspense fallback={<LoadingBlogPosts />}>
+                    <SuspenseBlogPosts {...props} />
+                </Suspense>
             </div>
         </Main>
     );
