141a813e63e0c6d0b2f3d36eb832b89e047726ff
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2025-03-19 15:18:08 +0000

    feat: update About page description

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -12,7 +12,7 @@ export const About = () => {
                 <PageTitle title="about" />
                 <div className="flex flex-1 items-center justify-center p-8 md:landscape:max-w-[90%]">
                     <div className="flex flex-col gap-8">
-                        <p className="text-justify">{`I am a software developer with over 2 years of experience developing software applications. As a developer, in the back end I have experience with the development and integration of API's, technologies related to NodeJS and TypeScript, framework NestJS, MongoDB, SQL and relational databases, git and object-oriented programming. In the front end, I have experience working with HTML, CSS, TypeScript/JavaScript and React. I also have some knowledge of CI/CD using Github actions and Docker.`}</p>
+                        <p className="text-justify">{`I am a software developer with over 3 years of experience developing software applications. As a developer, in the back end I have experience with the development and integration of API's, technologies related to NodeJS and TypeScript, framework NestJS, MongoDB, SQL and relational databases, git and object-oriented programming. In the front end, I have experience working with HTML, CSS, TypeScript/JavaScript and React. I also have some knowledge of CI/CD using Github actions and Docker.`}</p>
                         <p>{`My career path so far: `}</p>
                         <CareerPath />
                     </div>
