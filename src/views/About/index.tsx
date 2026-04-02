cc827a6057c734621ca2ecc33393ccc3f6de9bcc
marcus.castanho <castanhomarcus@gmail.com>
2026-04-02 11:55:24 -0300

    feat: add awesomic experience

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -12,7 +12,7 @@ export const About = () => {
                 <PageTitle title="about" />
                 <div className="flex flex-1 items-center justify-center p-8 md:landscape:max-w-[90%]">
                     <div className="flex flex-col gap-8">
-                        <p className="text-justify">{`I am a software developer with over 3 years of experience developing software applications. As a developer, in the back end I have experience with the development and integration of API's, technologies related to NodeJS and TypeScript, framework NestJS, MongoDB, SQL and relational databases, git and object-oriented programming. In the front end, I have experience working with HTML, CSS, TypeScript/JavaScript and React. I also have some knowledge of CI/CD using Github actions and Docker.`}</p>
+                        <p className="text-justify">{`I am a Full-stack and mobile developer with over 4 years of experience building web and mobile products end-to-end. I’ve delivered production apps and marketing sites from scratch, working closely with designers and founders while integrating backend services, authentication, AI features, and responsive UI. I focus on crafting intuitive user experiences, clean architecture, and shipping value quickly in fast-paced environments`}</p>
                         <p>{`My career path so far: `}</p>
                         <CareerPath />
                     </div>
